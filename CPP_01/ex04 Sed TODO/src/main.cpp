d9d6329003417c8764a3b42effcdefb66dfb9272
Kopeerde <kevin.mendesg@gmail.com>
2023-01-02 16:48:32 +0100

    - Worked a bit on sed. - Removed unnecessary clion folders.

--- CPP_01/ex04 Sed TODO/src/main.cpp
+++ CPP_01/ex04 Sed TODO/src/main.cpp
@@ -1,7 +1,16 @@
 
 #include "../headers/sed.hpp"
 
+void line_replacer()
+{
 
+}
+
+void line_checker(std::string line, char *search, char *replace)
+{
+	int i = 0; // Current index
+	int j = 0; // Previous index
+}
 
 int main(int argc, char **argv)
 {
@@ -13,15 +22,26 @@ int main(int argc, char **argv)
 	 */
 	if (argc == 4)
 	{
-		std::string s = argv[2];
-		if (s.compare("\0"))
+		std::string s = argv[2]; // Verification que ce que l'on remplace ne soit pas le charactere null
+		if (s.empty())
 			return 0;
-		s = argv[1];
-		s += ".replace";
-		std::ifstream src_file(argv[1]);
-		std::fstream dst_file("test");
 
-		dst_file << src_file.rdbuf();
+		std::string line; // Stockage de la ligne lue
+
+		s = argv[1]; // Stockage du nom pour le nouveau fichier
+		s += ".replace";
+
+		std::ifstream src_file(argv[1]); // Ouverture des fichiers sources et destinations
+		std::ofstream dst_file(s.c_str());
+
+		while (getline(src_file, line)) // Lecture des lignes et reecritures si necessaire
+		{
+			line_checker(line, argv[2], argv[3]);
+			dst_file << line << "\n";
+		}
+
+		src_file.close(); // Fermeture des fichiers sources et de destinations
+		dst_file.close();
 	}
 	return 0;
 }
