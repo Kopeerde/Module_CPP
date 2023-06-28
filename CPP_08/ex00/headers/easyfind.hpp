96550edb25f110e076bfec2b59c21271df8c3445
Kopeerde <kevin.mendesg@gmail.com>
2023-06-28 15:55:23 +0200

    Start of CPP08
    - Finished Module CPP08 ex00.

--- CPP_08/ex00/headers/easyfind.hpp
+++ CPP_08/ex00/headers/easyfind.hpp
@@ -1,12 +1,19 @@
 
-
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-template <class T>
-T *easyfind(T a, int b)
-{
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
+template <class T>
+void easyfind(T& a, int b)
+{
+	std::vector<int>::iterator res = std::find(a.begin(), a.end(), b);
+	if (res != a.end())
+		std::cout << "Element found at index : " << std::distance(a.begin(), res) << std::endl;
+	else
+		std::cout << "Element was not found in container." << std::endl;
 }
 
 #endif //EASYFIND_HPP
--- CPP_08/ex00/src/main.cpp
+++ CPP_08/ex00/src/main.cpp
@@ -1,10 +1,13 @@
-//
-// Created by kmendes- on 2/23/23.
-//
 
 #include "../headers/easyfind.hpp"
+#include <vector>
 
 int main()
 {
+	std::vector<int> vec(10, 0);
+	for (int i = 0; i < 10; i++)
+		vec[i] = i;
+	vec[1] = 5;
+	easyfind(vec, 5);
 	return 0;
 }
