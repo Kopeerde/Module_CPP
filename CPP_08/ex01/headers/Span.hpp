6a7b5bb4136c1edcf739006e659462be5347f5ce
Kopeerde <kevin.mendesg@gmail.com>
2023-06-30 12:30:14 +0200

    Sending CPP08 ex02
    - Exercice 02 from moudle CPP08 is finished.

--- CPP_08/ex01/headers/Span.hpp
+++ CPP_08/ex01/headers/Span.hpp
@@ -2,19 +2,25 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
 
 class Span
 {
 	private:
-
+		std::vector<int> vec;
+		size_t max_size;
 
 	public:
 		Span();
+		Span(size_t max_size);
 		~Span();
 		Span(const Span &original);
 		Span& operator=(const Span&);
 
-		void addNumber();
+		void addNumber(int value);
 		int shortestSpan();
 		int longestSpan();
 
--- CPP_08/ex01/src/Span.cpp
+++ CPP_08/ex01/src/Span.cpp
@@ -1,10 +1,49 @@
-//
-// Created by kmendes- on 2/23/23.
-//
 
 #include "../headers/Span.hpp"
 
-void Span::addNumber()
+Span::Span()
 {
+	this->max_size = 0;
+}
 
+Span::Span(size_t max_size)
+{
+	this->max_size = max_size;
+	this->vec = std::vector<int>();
+}
+
+
+Span::~Span()
+{
+	std::cout << "Span object destructor called." << std::endl;
+}
+
+// TODO : Surcharge opérateur = et constructeur par copie
+
+
+int Span::shortestSpan()
+{
+	int res = this->vec[1] - this->vec[0];
+
+	for (size_t i = 0; (i + 1) < this->vec.size(); i++)
+	{
+		if ((this->vec[i + 1] - this->vec[i]) < res)
+			res = this->vec[i + 1] - this->vec[i];
+	}
+
+	return res;
+}
+
+int Span::longestSpan()
+{
+	return (this->vec.back() - this->vec.front());
+}
+
+void Span::addNumber(int value)
+{
+	if (this->vec.size() >= this->max_size)
+		throw std::length_error("Max vector size length exceeded.");
+
+	this->vec.push_back(value);
+	std::sort(this->vec.begin(), this->vec.end());
 }
