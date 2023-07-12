d4fa9227324ebaeddeb28c242eab6adac4355fb3
Kopeerde <kevin.mendesg@gmail.com>
2023-07-12 20:50:28 +0200

    Finishing CPP09.

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -10,14 +10,106 @@
 
 #include "../headers/utils.hpp"
 
+void merge_lst(std::list<unsigned int> &lst, std::list<unsigned int> &top, std::list<unsigned int> &bottom)
+{
+	std::list<unsigned int>::iterator i = top.begin();
+	std::list<unsigned int>::iterator j = bottom.begin();
+
+	for (; i != top.end() && j != bottom.end(); ) // tant que l'ont n'as pas fini au moin une des sous-listes
+	{
+		if (*i < *j) // dereferencer donne la valeure
+		{
+			lst.push_back(*i);
+			i++;
+		}
+		else
+		{
+			lst.push_back(*j);
+			j++;
+		}
+	}
+
+	while (i != top.end()) // Append le reste si il y en as
+	{
+		lst.push_back(*i);
+		i++;
+	}
+	while (j != bottom.end()) // Comme precedemment mais avec l'autre sous-liste
+	{
+		lst.push_back(*j);
+		j++;
+	}
+}
+
 void sort_lst(std::list<unsigned int> &lst)
 {
-	(void) lst;
+	if (lst.size() < 2)
+		return ;
+	std::list<unsigned int> top, bottom;
+	unsigned long initial_size = lst.size();
+
+	for (; lst.size() != 0; lst.pop_front()) // Permet de separer les valeures en deux listes
+	{
+		if ((initial_size / 2) >= lst.size())
+			top.push_back(lst.front());
+		else
+			bottom.push_back(lst.front());
+	}
+	sort_lst(top);
+	sort_lst(bottom);
+
+	merge_lst(lst, top, bottom);
+}
+
+void merge_vec(std::vector<unsigned int> &lst, std::vector<unsigned int> &top, std::vector<unsigned int> &bottom)
+{
+	std::vector<unsigned int>::iterator i = top.begin();
+	std::vector<unsigned int>::iterator j = bottom.begin();
+
+	for (; i != top.end() && j != bottom.end(); )
+	{
+		if (*i < *j)
+		{
+			lst.push_back(*i);
+			i++;
+		}
+		else
+		{
+			lst.push_back(*j);
+			j++;
+		}
+	}
+
+	while (i != top.end())
+	{
+		lst.push_back(*i);
+		i++;
+	}
+	while (j != bottom.end())
+	{
+		lst.push_back(*j);
+		j++;
+	}
 }
 
 void sort_vec(std::vector<unsigned int> &vec)
 {
-	(void) vec;
+	if (vec.size() < 2)
+		return ;
+	std::vector<unsigned int> top, bottom;
+	unsigned long initial_size = vec.size();
+
+	for (; vec.size() != 0; vec.erase(vec.begin()))
+	{
+		if ((initial_size / 2) >= vec.size())
+			top.push_back(vec.front());
+		else
+			bottom.push_back(vec.front());
+	}
+	sort_vec(top);
+	sort_vec(bottom);
+
+	merge_vec(vec, top, bottom);
 }
 
 /**
@@ -52,10 +144,12 @@ int main(int argc, char **argv)
 	std::list<unsigned int> lst;
 	std::vector<unsigned int> vec;
 	std::vector<unsigned int> vec_buffer;
-	clock_t t;
+//	clock_t t;
+	timeval begin;
+	timeval end;
 
 	std::cout << "Before : ";
-	for (int i = 0; argv[i]; i++)
+	for (int i = 0; i < 5 && argv[i]; i++)
 		std::cout << argv[i] << " ";
 	std::cout << std::endl << "After : ";
 
@@ -71,17 +165,21 @@ int main(int argc, char **argv)
 	}
 	std::cout << std::endl;
 
-	t = clock();
+//	t = clock();
+	gettimeofday(&begin, NULL);
 	sort_lst(lst);
-	t = clock() - t;
+	gettimeofday(&end, NULL);
+	//	t = clock() - t;
 
 
-	std::cout << "Time to process a range of " << (argc - 1) << "elements with std::list : " << ((float) t / CLOCKS_PER_SEC) << std::endl;
+	std::cout << "Time to process a range of " << (argc - 1) << " elements with std::list : " << (end.tv_usec - begin.tv_usec) << " us" << std::endl;//((float) t / CLOCKS_PER_SEC) << std::endl;
 
-	t = clock();
+//	t = clock();
+	gettimeofday(&begin, NULL);
 	sort_vec(vec);
-	t = clock() - t;
-	std::cout << "Time to process a range of " << (argc - 1) << "elements with std::vector : " << ((float) t / CLOCKS_PER_SEC) << std::endl;
+	gettimeofday(&end, NULL);
+	//	t = clock() - t;
+	std::cout << "Time to process a range of " << (argc - 1) << " elements with std::vector : " << (end.tv_usec - begin.tv_usec) << " us" << std::endl;//((float) t / CLOCKS_PER_SEC) << std::endl;((float) t / CLOCKS_PER_SEC) << std::endl;
 
 	return 0;
 }
