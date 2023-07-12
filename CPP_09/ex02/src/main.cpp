bbfcc91a873f95efbbd3c93337f99197a04d36bd
Kopeerde <kevin.mendesg@gmail.com>
2023-07-12 14:41:39 +0200

    Changes to CPP09 ex02
    - Removed class
    ...... yup, that's all .........

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -1,12 +1,44 @@
 
 #include <iostream>
+#include <iomanip>
 #include <sys/time.h>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <stdlib.h>
+#include <limits.h>
 
-#include "../headers/PmergeMe.hpp"
+#include "../headers/utils.hpp"
 
+void sort_lst(std::list<unsigned int> &lst)
+{
+	(void) lst;
+}
 
+void sort_vec(std::vector<unsigned int> &vec)
+{
+	(void) vec;
+}
 
+/**
+ * Function created to populate both list and vector from user input arguments.
+ **/
+void populate(std::list<unsigned int> &lst, std::vector<unsigned int> &vec, char **lstr)
+{
+	long temp;
 
+	for (int i = 0; lstr[i]; i++)
+	{
+		temp = strtol(lstr[i], NULL, 10);
+		if (temp < 0 || temp > INT_MAX || !is_digits(lstr[i]))
+		{
+			std::cout << "Value is an int overflow or is negative or isn't a digit." << std::endl;
+			exit(1);
+		}
+		vec.push_back((unsigned int) temp);
+		lst.push_back((unsigned int) temp);
+	}
+}
 
 
 int main(int argc, char **argv)
@@ -17,12 +49,39 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	argv++;
-	PmergeMe tester = PmergeMe(argv);
+	std::list<unsigned int> lst;
+	std::vector<unsigned int> vec;
+	std::vector<unsigned int> vec_buffer;
+	clock_t t;
 
-	for (; *argv; argv++)
-		std::cout << *argv << " ";
+	std::cout << "Before : ";
+	for (int i = 0; argv[i]; i++)
+		std::cout << argv[i] << " ";
+	std::cout << std::endl << "After : ";
 
 
+	populate(lst, vec, argv);
+
+	vec_buffer = vec;
+	std::sort(vec_buffer.begin(), vec_buffer.end());
+	for (int i = 0; i < 5 && !vec_buffer.empty(); i++)
+	{
+		std::cout << vec_buffer.front() << " ";
+		vec_buffer.erase(vec_buffer.begin());
+	}
+	std::cout << std::endl;
+
+	t = clock();
+	sort_lst(lst);
+	t = clock() - t;
+
+
+	std::cout << "Time to process a range of " << (argc - 1) << "elements with std::list : " << ((float) t / CLOCKS_PER_SEC) << std::endl;
+
+	t = clock();
+	sort_vec(vec);
+	t = clock() - t;
+	std::cout << "Time to process a range of " << (argc - 1) << "elements with std::vector : " << ((float) t / CLOCKS_PER_SEC) << std::endl;
 
 	return 0;
 }
